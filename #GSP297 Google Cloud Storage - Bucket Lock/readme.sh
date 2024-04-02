3e23b816622b30e937e728860b7887037480907c
Shaquib Nawaid <63773478+shaquib01@users.noreply.github.com>
2024-04-02 18:02:44 +0530

    Create readme.sh

---
+++ #GSP297 Google Cloud Storage - Bucket Lock/readme.sh
@@ -0,0 +1,7 @@
+```
+curl -LO https://raw.githubusercontent.com/shaquib01/cloudskillsboost.google/master/%23GSP297%20Google%20Cloud%20Storage%20-%20Bucket%20Lock/gsp297.sh
+
+sudo chmod +x gsp297.sh 
+
+./gsp297.sh 
+````
