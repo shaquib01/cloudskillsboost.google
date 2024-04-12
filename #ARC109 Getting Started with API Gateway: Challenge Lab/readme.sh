7f368667983cde403b46f82bcae88ea310af4dd7
Shaquib Nawaid <63773478+shaquib01@users.noreply.github.com>
2024-04-12 10:54:36 +0530

    Create readme.sh

---
+++ #ARC109 Getting Started with API Gateway: Challenge Lab/readme.sh
@@ -0,0 +1,5 @@
+curl -LO https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Getting%20Started%20with%20API%20Gateway%20Challenge%20Lab/quicklabarc109.sh
+
+chmod +x arc109.sh
+
+./arc109.sh
