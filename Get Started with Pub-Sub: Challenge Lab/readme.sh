db426eccf0ea842579a8d6cd9d7ce9b0d2f9881b
Shaquib Nawaid <63773478+shaquib01@users.noreply.github.com>
2024-04-12 10:08:30 +0530

    Create readme.sh

---
+++ Get Started with Pub-Sub: Challenge Lab/readme.sh
@@ -0,0 +1,5 @@
+curl -LO https://raw.githubusercontent.com/shaquib01/cloudskillsboost.google/master/Get%20Started%20with%20Pub-Sub%3A%20Challenge%20Lab/arc113.sh
+
+sudo chmod +x quicklabarc113.sh
+
+./quicklabarc113.sh
