883a98e4baa1dd7a198d7b618fedef5dd1fe92ad
Shaquib Nawaid <63773478+shaquib01@users.noreply.github.com>
2024-05-11 22:32:04 -0700

    Create readme.sh

---
+++ #GSP851 BigQuery Machine Learning using Soccer Data/readme.sh
@@ -0,0 +1,4 @@
+curl -LO https://raw.githubusercontent.com/shaquib01/cloudskillsboost.google/master/%23GSP851%20BigQuery%20Machine%20Learning%20using%20Soccer%20Data/%23gsp851.sh
+sudo chmod +x gsp851.sh
+
+./sp851.sh
