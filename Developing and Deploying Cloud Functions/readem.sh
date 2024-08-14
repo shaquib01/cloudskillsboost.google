4b49e6baba1fe6808b641e4fb160eb4084ed4b16
Shaquib Nawaid <63773478+shaquib01@users.noreply.github.com>
2024-08-14 14:39:08 +0530

    Create readem.sh

---
+++ Developing and Deploying Cloud Functions/readem.sh
@@ -0,0 +1,10 @@
+```
+export REGION=
+```
+```
+curl -LO https://github.com/shaquib01/cloudskillsboost.google/raw/master/Developing%20and%20Deploying%20Cloud%20Functions/lab.sh
+
+sudo chmod +x lab.sh
+
+./lab.sh
+```
